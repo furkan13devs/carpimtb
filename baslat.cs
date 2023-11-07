61bab83251b825f22d92d8244ffe8776d816aae4
furkan13devs <121892854+furkan13devs@users.noreply.github.com>
2023-11-07 18:37:19 +0300

    Update baslat.cs

--- baslat.cs
+++ baslat.cs
@@ -1,9 +1,9 @@
 for (int j = 1; j <= 10; j++) // her sayımızı 1'den 10'a kadar çarptırma yaptığı kısım
                 {
-                    for (int i = 1; i <= 5; i++) // hangi sayıları 1'den 10'a kadar çarpacağı kısım burayı 5 yaptık ve en son değerimiz 50 oldu bu kısmı 10 yapsak 10 çarpı 10'dan 100 yapacak idi.
+                    for (int i = 1; i <= 5; i++) 
                     {
-                        Console.Write("{0}*{1}={2}\t", i, j, (i * j)); // ekran çıktısı aldığımız kısım 
+                        Console.Write("{0}*{1}={2}\t", i, j, (i * j)); 
                     }
-                    Console.WriteLine(); // alt alta sıralasın diye boşlukları atadığımız kısım
+                    Console.WriteLine();
                 }
                 Console.ReadKey();
